c64af9ed63a694bce236585670818aab48b6a13a
Lucas Vilaboim <lucas@vilaboim.com>
2022-11-03 18:24:25 -0300

    feat: add button component

---
+++ app/components/Button/Button.tsx
@@ -0,0 +1,38 @@
+interface Props {
+  children?: React.ReactNode;
+  full?: boolean;
+  primary?: boolean;
+  className?: string;
+  img?: React.ReactNode;
+  type?: "submit" | "reset" | "button";
+}
+
+type SetClassesParams = Omit<Props, 'children'> & {
+  hasImg: boolean;
+}
+
+function setClasses ({ primary, full, className = "", hasImg } : SetClassesParams) {
+  const colorClasses = primary ?
+    "bg-white text-[14px] text-black text-left border-2 border-black font-press" :
+    "bg-black text-[25px] text-white font-crux"
+
+    return `
+      h-[45px] rounded-md px-4
+      ${full && "w-full"}
+      ${colorClasses}
+      ${hasImg && 'flex justify-between items-center'}
+      ${className}
+    `;
+}
+
+export function Button({ children, full, primary, className, img, type = "button" }: Props) {
+  return (
+    <button
+      className={setClasses({ primary, full, className, hasImg: !!img})}
+      type={type}
+    >
+      {children}
+      {img}
+    </button>
+  );
+}
