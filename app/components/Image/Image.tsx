2d8d5f837d880cf6f5b06d0d05fc6a0519345caf
Lucas Vilaboim <lucas@vilaboim.com>
2022-11-04 17:23:54 -0300

    fix: add image max height

--- app/components/Image/Image.tsx
+++ app/components/Image/Image.tsx
@@ -1,12 +1,14 @@
 interface Props {
   alt: string;
   title?: string;
-  src: string;
+  src?: string;
+  className?: string;
 }
 
-export function Image({ alt, title, src }: Props) {
+export function Image({ alt, title, src, className = '' }: Props) {
   return (
     <img
+      className={`h-full ${className}`}
       src={src}
       alt={alt}
       title={title || alt}
