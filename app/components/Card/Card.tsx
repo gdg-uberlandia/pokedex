abefd7cad7a0e6040b892389f250c895e3d0965c
Lucas Vilaboim <lucas@vilaboim.com>
2022-11-04 17:23:32 -0300

    fix: change card bg

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -6,12 +6,12 @@ interface Props {
 
 export function Card ({ children, title, className = '' }: Props) {
   return (
-    <section className={`w-full overflow-hidden bg-white rounded-t ${className}`}>
+    <section className={`w-full overflow-hidden rounded-t ${className}`}>
       <h3 className="flex h-9 items-center justify-center border-2 border-black bg-black font-press text-center text-sm font-normal text-white">
         {title}
       </h3>
 
-      <div className="p-3.5 border-2 border-black rounded-b">{children}</div>
+      <div className="p-3.5 bg-white border-2 border-black rounded-b">{children}</div>
     </section>
   );
 }
