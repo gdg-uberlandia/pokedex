f3d9fd087cb0d7b2fc4aa229cb172a9e9fa83200
Tacio Medeiros <taciomedeiros@gmail.com>
2022-11-08 10:10:21 -0300

    Register user (#1)
    * feat: creating features structures and separating service for firebase
    
    * fix: removing consoles
    
    * feat: finalizacao da sincronia com o perfil pelo codigo
    
    * chore: remove console.log
    
    * fix: typo registrationId
    
    * fix: login.tsx
    
    Co-authored-by: Tacio Medeiros <tacio.medeiros@vitta.me>

---
+++ app/routes/api/validate.tsx
@@ -0,0 +1,35 @@
+import type { ActionFunction } from "@remix-run/node"; // or cloudflare/deno
+import { json } from "@remix-run/node";
+
+import { cors } from 'remix-utils'
+import { consumeAward, getAwardById } from "~/features/awards/awards.server";
+
+
+
+export const action: ActionFunction = async ({
+    request,
+}) => {
+
+    const payload = await request.json();
+
+    if (payload.code == null) {
+        return await cors(request, json({ message: "Invalid Code" }, 400));
+    }
+
+    const award = await getAwardById(payload.code)
+
+    if (!award) {
+        return await cors(request, json({ message: "Invalid Code" }, 400));
+    }
+
+    await consumeAward(payload.code);
+    switch (request.method) {
+        case "POST": {
+            return await cors(request, json({
+                success: !award.consumed,
+                code: payload.code,
+            }, 200));
+        }
+    }
+};
+
--- app/utils/collections.ts
+++ app/utils/collections.ts
@@ -1,3 +1,4 @@
 export const COLLECTIONS = {
   PROFILES: "profiles",
+  AWARDS: 'awards',
 };
