2eb92b04869b5c8f6ce6137565d311899d5d33c4
Lucas Vilaboim <lucas@vilaboim.com>
2022-11-08 10:18:01 -0300

    style: run prettier

--- app/features/profiles/profile.server.ts
+++ app/features/profiles/profile.server.ts
@@ -1,4 +1,3 @@
-
 import { db } from "~/services/firebase.server"
 import { COLLECTIONS } from "~/utils/collections";
 import type { Profile } from "./types";
@@ -20,9 +19,13 @@ export const getById = async (id: string): Promise<Profile> => {
 };
 
 //https://github.com/ianlenehan/my-remix-app/blob/master/app/post.js
-export const getProfileByCode = async (code: string): Promise<Profile | null> => {
-
-  const querySnapshot = await db.collection(COLLECTIONS.PROFILES).where("code", "==", code).get();
+export const getProfileByCode = async (
+  code: string
+): Promise<Profile | null> => {
+  const querySnapshot = await db
+    .collection(COLLECTIONS.PROFILES)
+    .where("code", "==", code)
+    .get();
 
   const data: Array<Profile> = [];
   querySnapshot.forEach((doc: any) => {
