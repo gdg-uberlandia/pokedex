052e7cfa1d45cfb5d5432d6b40ccef0150adc854
Lucas Vilaboim <lucas@vilaboim.com>
2022-11-24 19:48:26 -0300

    feat(profile): integrate with api

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,52 +2,59 @@ import type { LoaderArgs } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Form, useLoaderData, Link } from "@remix-run/react";
-import { getUser } from "~/features/users/user.server";
-import { Button, Card, Image, Profile, Tags } from "~/components";
+import { Button, Card, Image, Profile, Skills } from "~/components";
 import pokeball from "~/assets/images/pokeball.png";
-import clock from "~/assets/images/clock.svg";
 import { ROUTES } from "~/utils/routes";
-
-type LoaderData = {
-  data: Awaited<ReturnType<typeof getUser>>;
-};
+import { getSession } from "~/features/users/session.server";
+import { getById } from "~/features/profiles/profile.server";
 
 export async function loader({ request }: LoaderArgs) {
-  const data = await getUser(request);
-  return json<LoaderData>({ data });
+  const session = await getSession(request.headers.get("Cookie"));
+  const data = await getById(session.get("userId"));
+
+  return json(data);
 }
 
 export default function Index() {
-  const { data } = useLoaderData<typeof loader>();
+  const { user, skills } = useLoaderData();
 
-  if (!data) {
+  if (!user) {
     throw redirect(ROUTES.LOGIN);
   }
 
   return (
     <>
-      <Card title="Seu perfil" className="mb-7">
-        <Profile image={data.picture} name={data.name} isAvatar />
-        <Tags tags={["#flutter", "#leadership"]} />
+      <Card className="mb-7" title="Seu perfil">
+        <Profile
+          className="mb-1"
+          image={user.photoUrl}
+          isAvatar
+          name={user.name}
+        />
+        <Skills content={skills} className="mb-2" />
+
+        <Link to={ROUTES.UPDATE_PROFILE}>
+          <Button full>Atualizar perfil</Button>
+        </Link>
       </Card>
 
       <Link to={ROUTES.POKEDEX_PEOPLE}>
         <Button
           className="mb-4"
+          full
           img={<Image src={pokeball} alt="Ver Pokédex" className="h-full" />}
           primary
-          full
         >
           Ver Pokédex
         </Button>
       </Link>
 
       <Form
-        method="post"
         action="/logout"
         className="absolute inset-x-0 bottom-4"
+        method="post"
       >
-        <Button type="submit" full>
+        <Button full type="submit">
           Sair
         </Button>
       </Form>
--- app/utils/routes.ts
+++ app/utils/routes.ts
@@ -1,6 +1,6 @@
 export const ROUTES = {
   HOME: "/",
-  REGISTER: "/register",
   LOGIN: "/login",
   POKEDEX_PEOPLE: "/pokedex/people",
+  UPDATE_PROFILE: "/update",
 };
