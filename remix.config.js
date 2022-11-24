3656b67e71ad1c1e3daffbf14bffb1a9c02600a3
Tacio Medeiros <tacio.medeiros@vitta.me>
2022-11-24 17:42:46 -0300

    feat: correcao remixjs config

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   serverBuildTarget: "vercel",
-  server:
-    process.env.NETLIFY || process.env.NETLIFY_LOCAL
-      ? "./server.js"
-      : undefined,
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
