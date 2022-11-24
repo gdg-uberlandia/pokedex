e862d500e5d869a486f797d015ba490e6455fb00
Lucas Vilaboim <lucas@vilaboim.com>
2022-11-24 19:29:30 -0300

    feat(inputgroup): add component InputGroup

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,6 +2,7 @@ export { Button } from "./Button";
 export { Card } from "./Card";
 export { Image } from "./Image";
 export * from "./Input";
+export * from "./InputGroup";
 export { Logo } from "./Logo";
 export { Profile } from "./Profile";
 export { Tags } from "./Tags";
