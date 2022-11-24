122c131034b24e989ae86ef3a3f2ece5bc22ef21
Tacio Medeiros <tacio.medeiros@vitta.me>
2022-11-24 18:03:23 -0300

    fix: corigindo deploy vercel

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,3 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "vercel",
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  ignoredRouteFiles: ["**/.*"],
 };
