bf9ff1da3f39d88f7c8ac561e44c179de05d7617
Tacio Medeiros <tacio.medeiros@vitta.me>
2022-11-24 17:59:23 -0300

    feat: adicionando remix config

--- remix.config.js
+++ remix.config.js
@@ -3,8 +3,4 @@ module.exports = {
   serverBuildTarget: "vercel",
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  assetsBuildDirectory: "public/build/",
-  serverBuildPath: "public/build/",
-  publicPath: "public/",
 };
