7aedfe195f989ea2dfe0fb0aad62fb9dfe0e0ae0
Vitor Resende <vresende@ciandt.com>
2022-11-25 15:57:56 -0300

    feat: integrate profiles and companies with user data

--- app/routes/pokedex/companies.tsx
+++ app/routes/pokedex/companies.tsx
@@ -1,29 +1,32 @@
+import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
 import { Profile } from "~/components";
-import { getCompanies } from "~/models/pokedex.server";
+import { getProfileByEmail } from "~/features/profiles/profile.server";
+import { getUser } from "~/features/users/user.server";
 
 type LoaderData = {
-  data: Awaited<ReturnType<typeof getCompanies>>;
+  data: Awaited<ReturnType<typeof getUser>>;
+  profile: Awaited<ReturnType<typeof getProfileByEmail>>;
 };
 
-export async function loader() {
-  const data = await getCompanies();
-
-  return json<LoaderData>({ data });
+export async function loader({ request }: LoaderArgs) {
+  const data = await getUser(request);
+  const profile = await getProfileByEmail(data.email || "");
+  return json<LoaderData>({ data, profile });
 }
 
 export default function Companies() {
-  const { data } = useLoaderData<typeof loader>();
+  const { profile } = useLoaderData<typeof loader>();
 
-  if (!data) {
+  if (!profile) {
     return null;
   }
 
   return (
     <div className="grid grid-cols-2 gap-4">
-      {data.map((data, key) => (
+      {profile?.contents?.companies.map((data, key) => (
         <a key={key} href={data.url} target="_blank" rel="noreferrer">
           <Profile image={data.image} name={data.name} className="h-full" />
         </a>
