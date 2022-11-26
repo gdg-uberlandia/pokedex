ba4077265c2c24473d416a5d0d2a9742451b93c4
Tacio Medeiros <tacio.medeiros@vitta.me>
2022-11-26 05:54:49 -0300

    fix: adjusting levels

--- app/utils/levels.ts
+++ app/utils/levels.ts
@@ -1,20 +1,11 @@
 export const LEVELS = {
-    1: 10,
-    2: 20,
-    3: 40,
-    4: 7400,
-};
-
-/*
-export const LEVELS = {
     1: 700,
     2: 3200,
     3: 4400,
     4: 7400,
     5: 11900,
     6: 10000000,
-};*/
-
+};
 
 export const getLevelByScore = (score: number): number => {
 
