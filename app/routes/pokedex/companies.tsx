731dbd06fbe1c3be38b9d2405fe34faf90194789
Lucas Vilaboim <lucas@vilaboim.com>
2023-09-29 19:47:23 -0300

    feat: update people and companies code

--- app/routes/pokedex/companies.tsx
+++ app/routes/pokedex/companies.tsx
@@ -7,38 +7,33 @@ import { getProfileByEmail } from "~/features/profiles/profile.server";
 import { getUser } from "~/features/users/user.server";
 
 type LoaderData = {
-  data: Awaited<ReturnType<typeof getUser>>;
   profile: Awaited<ReturnType<typeof getProfileByEmail>>;
 };
 
 export async function loader({ request }: LoaderArgs) {
   const data = await getUser(request);
   const profile = await getProfileByEmail(data.email || "");
-  return json<LoaderData>({ data, profile });
+
+  return json<LoaderData>({ profile });
 }
 
 export default function Companies() {
   const { profile } = useLoaderData<typeof loader>();
+  const contents = profile?.contents;
 
-  if (!profile) {
-    return null;
+  if (!contents?.companies.length) {
+    return (
+      <h4 className="font-press text-sm">Nenhuma empresa na sua Pokédex</h4>
+    );
   }
 
   return (
-    <>
-      {profile?.contents?.companies?.length ? (
-        <div className="grid grid-cols-2 gap-4">
-          {profile?.contents?.companies?.map((data, key) => (
-            <a key={key} href={data.url} target="_blank" rel="noreferrer">
-              <Profile image={data.image} name={data.name} className="h-full" />
-            </a>
-          ))}
-        </div>
-      ) : (
-        <p style={{ fontFamily: "PressStart", padding: 10 }}>
-          Nenhum empresa na sua Pokédex.
-        </p>
-      )}
-    </>
+    <div className="grid grid-cols-2 gap-4">
+      {profile?.contents?.companies?.map((data, key) => (
+        <a key={key} href={data.url} target="_blank" rel="noreferrer">
+          <Profile image={data.image} name={data.name} className="h-full" />
+        </a>
+      ))}
+    </div>
   );
 }
